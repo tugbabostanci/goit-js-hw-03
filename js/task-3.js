e376b7d705996522d1afa7378849aa7223eb109b
tugbabostanci <ttugbabostanci@gmail.com>
2025-07-19 12:41:34 +0300

    task-3

--- js/task-3.js
+++ js/task-3.js
@@ -2,19 +2,21 @@
 
 
 
-function checkForSpam (message) {
-    const spam = "spam";
-    const sale = "sale";
-     return message.toLowerCase(). includes(spam.toLowerCase()) || message.toLowerCase(). includes(sale.toLowerCase());
+function  filterArray(numbers, value) {
+  const emptArray = [];
+
+  for (const number of numbers) {
+    if (number > value) {
+      emptArray.push(number);
+    }
+  }
+
+  return emptArray;
 }
 
 
-
-console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
-
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
